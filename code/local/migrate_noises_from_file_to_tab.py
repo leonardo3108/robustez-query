fee2e3f99d1bf87ce60e0ede5c3ab27e2d46dee9
MARCUS VINÍCIUS BORELA DE CASTRO <borela@tcu.gov.br>
2021-11-21 12:16:33 -0300

    Migrated noises queries from file to tab

---
+++ code/local/migrate_noises_from_file_to_tab.py
@@ -0,0 +1,33 @@
+"""
+Migrate noise 
+from 
+    noisy-queries-translation-pt.txt and noisy-queries-typo.txt
+to 
+    tab_noisy_query.csv
+"""
+import util_bd_dataframe  as util_bd_pandas
+
+df_original_query = util_bd_pandas.read_df_original_query()
+
+
+# migrating noisy-queries-translation-pt
+noisy_text_list = {'cod_original_query': [], 'text': []}
+for line in open('data/noisy-queries-translation-pt.txt'):
+    fields = line.strip().split()
+    cod_query = fields[0]
+    text_query = ' '.join(fields[1:])
+    noisy_text_list['cod_original_query'].append(cod_query)
+    noisy_text_list['text'].append(text_query)
+
+util_bd_pandas.save_noisy_query(noisy_text_list,7, "Back translation (with portuguese)")
+
+# migrating noisy-queries-typo-pt
+noisy_text_list = {'cod_original_query': [], 'text': []}
+for line in open('data/noisy-queries-typo.txt'):
+    fields = line.strip().split()
+    cod_query = fields[0]
+    text_query = ' '.join(fields[1:])
+    noisy_text_list['cod_original_query'].append(cod_query)
+    noisy_text_list['text'].append(text_query)
+
+util_bd_pandas.save_noisy_query(noisy_text_list,8, "With one typographical error")
