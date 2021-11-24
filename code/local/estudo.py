04268b27a2c1f876bc83ab9f834331307f41952d
MARCUS VINÍCIUS BORELA DE CASTRO <borela@tcu.gov.br>
2021-11-24 06:05:10 -0300

    calculated all noise kind for search context 5

--- code/local/estudo.py
+++ code/local/estudo.py
@@ -1,12 +1,16 @@
 """
 just to explore coding
 """
+print('oi')
 
-import pandas as pd
-import numpy as np
-import util_bd_dataframe  as util_bd_pandas
-import time
+# import googletrans
+# print(googletrans.LANGUAGES)
 
-print("oi")
-df = util_bd_pandas.read_df_calculated_metric_with_label()
-util_bd_pandas.imprime_resumo_df(df)
+from googletrans import Translator
+
+translator = Translator()
+texto_origem = 'eu te amo'
+lingua_origem = 'pt'
+lingua_destino = 'en'
+texto_traduzido = translator.translate('eu te amo',dest=lingua_destino,src=lingua_origem)
+print(texto_traduzido.text)
